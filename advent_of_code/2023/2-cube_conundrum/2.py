3acfd6ebcc0faa83391fe50ebbbb335921b2e59e
Luis Lara <luis@MacBook-Air-de-Luis.local>
2023-12-05 20:58:39 -0600

    Add AoC 2023 day 2

---
+++ advent_of_code/2023/2-cube_conundrum/2.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+
+with open("input.txt", "r") as input_file:
+    games = input_file.readlines()
+
+result = 0
+for game in games:
+    red = green = blue = 0
+    head, tail = game.split(":")
+    game_id = int(head.split()[1])
+    for turn in tail.split(";"):
+        splitline = [s.split() for s in turn.split(",")]
+        sets = {x[1].strip(): int(x[0].strip()) for x in splitline}
+        red = max(red, sets.get("red", 0))
+        green = max(green, sets.get("green", 0))
+        blue = max(blue, sets.get("blue", 0))
+    power = red * green * blue
+    result += power
+print(result)
